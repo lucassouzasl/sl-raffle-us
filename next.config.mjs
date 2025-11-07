d500a34df420b70b5c6bddb270f10ebaed48f6c3
Lucas <ls4803326@gmail.com>
2025-11-07 16:42:41 -0300

    config: remove domains decapred, add remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com", "drive.google.com"],
+    // domains: ["source.unsplash.com", "drive.google.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "**",
+      },
+    ],
   },
   eslint: {
     ignoreDuringBuilds: true,
